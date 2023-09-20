d08e724d57887443a05c5b2bdbf280934de9d5dc
QiuRongChen2023 <121112820+QiuRongChen2023@users.noreply.github.com>
2023-09-19 21:28:21 -0400

    Update BarPlot_df.R

--- BarPlot_df.R
+++ BarPlot_df.R
@@ -6,22 +6,22 @@
          geom_bar(stat = "identity", fill = "blue") + 
          labs(title = "Unemployment Rate Data Bar Chart", 
          x = "Country", y = "Unemployment rate") + 
-         theme(axis.text.x = element_text(angle = 45, hjust = 1))
+         theme(axis.text.x = element_text(angle = 45, hjust = 1)) + coord_flip()
 
  ggplot(df,aes(x = Country, y = `Out of pocket health expenditure`)) + 
          geom_bar(stat = "identity", fill = "red") + 
          labs(title = "Health Expenditure Data Bar Chart", 
          x = "Country", y = "Health expenditure") + 
-         theme(axis.text.x = element_text(angle = 45, hjust = 1))
+         theme(axis.text.x = element_text(angle = 45, hjust = 1)) + coord_flip()
 
  ggplot(df,aes(x = Country, y = `Gross primary education enrollment (%)`)) + 
          geom_bar(stat = "identity", fill = "green") + 
          labs(title = "Primary Education Enrollment Data Bar Chart", 
          x = "Country", y = "primary education enrollment") + 
-         theme(axis.text.x = element_text(angle = 45, hjust = 1))
+         theme(axis.text.x = element_text(angle = 45, hjust = 1)) + coord_flip()
 
  ggplot(df,aes(x = Country, y = GDP)) + 
          geom_bar(stat = "identity", fill = "yellow") + 
          labs(title = "GDP Data Bar Chart", 
          x = "Country", y = "GDP") + 
-         theme(axis.text.x = element_text(angle = 45, hjust = 1))
+         theme(axis.text.x = element_text(angle = 45, hjust = 1)) + coord_flip()
