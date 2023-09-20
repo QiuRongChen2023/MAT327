545cc038a9cc2a69b3e5b1229852f0d0eb29a630
QiuRongChen2023 <121112820+QiuRongChen2023@users.noreply.github.com>
2023-09-20 15:18:53 -0400

    Add files via upload

---
+++ world_data_plot.R
@@ -0,0 +1,13 @@
+
+hist(world_data_2023$`Physicians per thousand`,
+                  main = "Histogram of Physicians per thousand 2023",
+                  xlab = ("Physicians per thousand"), ylab="Num of country" )
+hist(world_data_2023$`Maternal mortality ratio`,
+                  main = "Histogram of number of maternal deaths per 100,000 live births",
+                  xlab = ("Number of maternal deaths per 100,000 live births"), ylab = "Num of country")
+hist(world_data_2023$`Life expectancy`,
+                  main = "Histogram of average life expectancy", 
+                  xlab = ("Average life expectancy"),ylab = "Num of country")
+hist(world_data_2023$`Birth Rate`,
+                  main = "Histogram of birth rate", 
+                  xlab = ("Number of births per 1,000 population per yeary"),ylab = "Num of country")
